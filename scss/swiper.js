62d71fbd55c7c9f8bfdcbfb44a2f4201661cd443
FIRST_NAME LAST_NAM <andrius.braskys1993@gmail.com>
2023-05-07 23:24:07 +0300

    6

---
+++ scss/swiper.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+  
+  // Change button style
+  const buttons = document.getElementsByTagName("button")
+  swiper.on("slideChange", function(){
+    // Removes active class from buttons
+    for(let button of buttons){
+      button.classList.remove("active")
+    }
+  
+    // Add active class according to active index
+    let index = swiper.activeIndex
+    buttons[index].classList.add("active")
+  })
+  
+  // Change slide according to button index
+  for(let button of buttons){
+    button.addEventListener("click", function() {
+      swiper.slideTo(this.getAttribute("index"))
+    })
+  }
